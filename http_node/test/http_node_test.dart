684dd68e1b08276eca827f154f6e277ccaef853a
alex <alex@tekartik.com>
2019-03-13 11:01:38 +0100

    test: skip github access on travis for node

--- http_node/test/http_node_test.dart
+++ http_node/test/http_node_test.dart
@@ -3,18 +3,20 @@ library tekartik_http_node.http_node_test;
 
 import 'dart:convert';
 
+import 'package:node_io/node_io.dart';
 import 'package:tekartik_http_node/http_node.dart';
 import 'package:tekartik_http_test/http_test.dart';
 import 'package:test/test.dart';
-//import 'dart:node';
+
+bool get runningOnTravis => Platform.environment['TRAVIS'] == "true";
 
 void main() {
   run(httpFactoryNode);
   test('connected', () async {
     var client = httpFactoryNode.client.newClient();
     var content = await client.read('https://api.github.com',
-        headers: {'User-Agent': 'tekarik_http_node'});
+        headers: {'User-Agent': 'tekartik_http_node'});
     var map = jsonDecode(content);
     expect(map['current_user_url'], 'https://api.github.com/user');
-  });
+  }, skip: runningOnTravis);
 }
