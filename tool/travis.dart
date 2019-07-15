e4a74e4061091aeb1763b82bb7e68721ce98d8ee
alex <alex@tekartik.com>
2019-07-15 17:17:03 +0200

    test: re-allow node test

--- tool/travis.dart
+++ tool/travis.dart
@@ -1,24 +1,15 @@
-import 'dart:io';
-
 import 'package:process_run/shell.dart';
-import 'package:pub_semver/pub_semver.dart';
-
-import 'tool_test.dart';
 
 Future main() async {
   var shell = Shell();
 
-  var dartVersion = parsePlatformVersion(Platform.version);
-  bool oldListInt = dartVersion <= Version(2, 5, 0, pre: 'dev');
   for (var dir in [
     'http',
     'http_browser',
     'http_io',
-    // TODO temp node skipping
-    if (oldListInt) 'http_node',
+    'http_node',
     'http_test',
-    // TODO temp node skipping
-    if (oldListInt) 'http_redirect',
+    'http_redirect',
   ]) {
     shell = shell.pushd(dir);
     await shell.run('''
