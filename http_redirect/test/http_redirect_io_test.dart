393b9c7afa96e754bc182bcf96c7bcf4e2a01257
alex <alex@tekartik.com>
2019-09-20 15:34:33 +0200

    build: bump sdk dependencies

--- http_redirect/test/http_redirect_io_test.dart
+++ http_redirect/test/http_redirect_io_test.dart
@@ -3,7 +3,7 @@ library tekartik_http_redirect.test.http_redirector_io_test;
 
 import 'package:tekartik_http_io/http_io.dart';
 import 'package:test/test.dart';
-import 'http_redirect_test.dart';
+import 'multiplatform/http_redirect_test.dart';
 
 void main() {
   run(httpFactoryIo);
