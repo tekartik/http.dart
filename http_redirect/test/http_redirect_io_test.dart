94b13855d8cba435c06783195c06a878586cff67
alex <alex@tekartik.com>
2021-08-26 15:00:25 +0200

    feat: deprecate http_redirect

--- http_redirect/test/http_redirect_io_test.dart
+++ http_redirect/test/http_redirect_io_test.dart
@@ -1,11 +1,14 @@
 @TestOn('vm')
 library tekartik_http_redirect.test.http_redirector_io_test;
 
+import 'package:tekartik_http/http_memory.dart';
 import 'package:tekartik_http_io/http_io.dart';
 import 'package:test/test.dart';
 
 import 'multiplatform/http_redirect_test.dart';
 
 void main() {
-  run(httpFactoryIo);
+  run(httpFactory: httpFactoryIo);
+  run(httpFactory: httpFactoryIo, testServerHttpFactory: httpFactoryMemory);
+  run(httpFactory: httpFactoryMemory, testServerHttpFactory: httpFactoryIo);
 }
--- http_redirect/test/multiplatform/http_redirect_test.dart
+++ http_redirect/test/multiplatform/http_redirect_test.dart
@@ -1,16 +1,24 @@
 import 'package:tekartik_http/http.dart';
 import 'package:tekartik_http/http_memory.dart';
-import 'package:tekartik_http_redirect/http_redirect.dart';
+import 'package:tekartik_http_redirect/http_redirect.dart'; // ignore: deprecated_member_use_from_same_package
 import 'package:test/test.dart';
 
 void main() {
-  run(httpFactoryMemory);
+  run(httpFactory: httpFactoryMemory);
 }
 
-void run(HttpFactory factory) {
+void run({
+  HttpFactory? httpFactory,
+  //HttpClientFactory? httpClientFactory,
+  //HttpServerFactory? httpServerFactory,
+  HttpFactory? testServerHttpFactory,
+}) {
+  var clientFactory = testServerHttpFactory?.client ?? httpFactory!.client;
+  var serverFactory = httpFactory!.server;
   group('redirect', () {
     test('redirect', () async {
-      var httpServer = await factory.server.bind(localhost, 0);
+      var httpServer = await (testServerHttpFactory?.server ?? serverFactory)
+          .bind(localhost, 0);
 
       httpServer.listen((HttpRequest request) {
         request.response
@@ -20,14 +28,15 @@ void run(HttpFactory factory) {
       var port = httpServer.port;
       //devPrint('port: $port');
 
-      var httpRedirectServer = await startServer(
-          factory.server,
-          Options()
+      var httpRedirectServer = await HttpRedirectServer.startServer(
+          httpClientFactory: testServerHttpFactory?.client ?? clientFactory,
+          httpServerFactory: serverFactory,
+          options: Options()
             ..host = localhost
             ..port = 0
             ..baseUrl = 'http://$localhost:$port');
 
-      var client = factory.client.newClient();
+      var client = clientFactory.newClient();
       var redirectPort = httpRedirectServer.port;
       //devPrint('redirectPort: $redirectPort');
       expect(port, isNot(redirectPort));
