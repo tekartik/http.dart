555a3a9e7418a4161c3e8e5bc15c71c2d8abc235
alex <alex@tekartik.com>
2023-05-12 16:37:09 +0200

    feat: add basic auth client

---
+++ http/lib/src/basic_auth_client.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+/// Http client holding a username and password to be used for Basic authentication
+class BasicAuthClient extends http.BaseClient {
+  /// The username to be used for all requests
+  final String username;
+
+  /// The password to be used for all requests
+  final String password;
+
+  final http.Client _inner;
+  final String _authString;
+
+  /// Creates a client wrapping [inner] that uses Basic HTTP auth.
+  ///
+  /// Constructs a new [BasicAuthClient] which will use the provided [username]
+  /// and [password] for all subsequent requests.
+  BasicAuthClient(this.username, this.password, {required http.Client inner})
+      : _authString = _getAuthString(username, password),
+        _inner = inner;
+
+  static String _getAuthString(String username, String password) {
+    final token = base64.encode(latin1.encode('$username:$password'));
+
+    final authstr = 'Basic ${token.trim()}';
+
+    return authstr;
+  }
+
+  void _setAuthString(http.BaseRequest request) {
+    var authMap = <String, String>{'Authorization': _authString};
+    request.headers.addAll(authMap);
+  }
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) async {
+    _setAuthString(request);
+    return _inner.send(request);
+  }
+}
