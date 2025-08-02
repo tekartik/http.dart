b52a858bd281b13a36adf9d80c30287740133c5e
Alexandre Roux <alex@tekartik.com>
2025-08-02 23:58:31 +0200

    fix deps

--- http_redirect/lib/http_redirect.dart
+++ http_redirect/lib/http_redirect.dart
@@ -4,6 +4,7 @@ library;
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as p;
 import 'package:tekartik_common_utils/common_utils_import.dart';
+import 'package:tekartik_common_utils/log_utils.dart';
 import 'package:tekartik_http/http.dart';
 import 'package:tekartik_http_redirect/src/http_redirect_server.dart';
 
